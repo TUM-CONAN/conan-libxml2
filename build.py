57d249ee98f7b9348f29cad7a06b8dde094d5efb
Julien Waechter <julien.waechter@gmail.com>
2018-07-27 12:00:47 +0200

    chore(build): remove visual versions in multi-packager script

--- build.py
+++ build.py
@@ -4,7 +4,6 @@ if __name__ == "__main__":
     builder = ConanMultiPackager(
         username="fw4spl", 
         visual_runtimes=["MD", "MDd"],
-        visual_versions=["14","15"],
         archs=["x86_64"])
     builder.add_common_builds(shared_option_name=False, pure_c=True)
     builder.run()
