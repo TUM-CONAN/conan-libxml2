c94c2f0d849d0af2f53381644fe66044e74c0a99
Didier WECKMANN <didier.weckmann@ircad.fr>
2019-06-16 20:30:34 +0200

    fix(conanfile): test

--- conanfile.py
+++ conanfile.py
@@ -79,8 +79,8 @@ class LibxmlConan(ConanFile):
             cmake = CMake(self)
             
             # Set common flags
-            cmake.definitions["SIGHT_CMAKE_C_FLAGS"] = common.get_c_flags() + " /TP "
-            cmake.definitions["SIGHT_CMAKE_CXX_FLAGS"] = common.get_cxx_flags() + " /TP "
+            cmake.definitions["SIGHT_CMAKE_C_FLAGS"] = common.get_c_flags()
+            cmake.definitions["SIGHT_CMAKE_CXX_FLAGS"] = common.get_cxx_flags()
             
             cmake.configure(build_folder=self.build_subfolder)
             cmake.build()
